ab84b99268dc7061c5b08e46f12c7bd4d00296e5
Zain Ul Abdeen <zainulabdeen.ibs@gmail.com>
2024-02-17 04:32:43 +0500

    feat: add students CRUD API's

---
+++ src/Services/Student/Student.DTO/StudentPostDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Student.DTO
+{
+    public class StudentPostDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string ZipCode { get; set; } = string.Empty;
+    }
+}
---
+++ src/Services/Student/Student.DTO/StudentUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Student.DTO
+{
+    public class StudentUpdateDto
+    {
+        public Guid Id { get; set; }
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string ZipCode { get; set; } = string.Empty;
+    }
+}
