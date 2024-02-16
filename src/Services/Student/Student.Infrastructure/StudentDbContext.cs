c59940552e61c72a6b9adcbf40482ff94fa33aee
Zain Ul Abdeen <zainulabdeen.ibs@gmail.com>
2024-02-17 04:31:30 +0500

    feat: add student db context

---
+++ src/Services/Student/Student.Infrastructure/StudentDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EntityFrameworkCore.Triggers;
+using Microsoft.EntityFrameworkCore;
+
+namespace Student.Infrastructure
+{
+    public class StudentDbContext : DbContextWithTriggers
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Domain.Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("student");
+        }
+    }
+}
