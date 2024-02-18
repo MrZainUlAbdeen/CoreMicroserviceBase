2a15a3723add6fc4a233a4da2638cf4dd81e8f99
Zain Ul Abdeen <zainulabdeen.ibs@gmail.com>
2024-02-18 18:34:14 +0500

    chore: used class object

--- src/CoreMicroserviceCommon/Repositories/TriggersBaseRepository.cs
+++ src/CoreMicroserviceCommon/Repositories/TriggersBaseRepository.cs
@@ -1,12 +1,11 @@
-﻿using BaseTriggers;
-using EntityFrameworkCore.Triggers;
+﻿using EntityFrameworkCore.Triggers;
 
 namespace Repositories
 {
     public class TriggersBaseRepository<TEntity, TDbContext> :
         BaseRepository<TEntity, TDbContext>,
         ITriggersBaseRepository<TEntity>
-        where TEntity : BaseEntity
+        where TEntity : class
         where TDbContext : DbContextWithTriggers
     {
         public TriggersBaseRepository(TDbContext dbContext) : base(dbContext)
